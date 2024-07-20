bb91b9873c6ccaafa368a469f69140b305327aea
Stefan <SDreyer@dvtsoftware.com>
2024-07-20 11:22:15 +0200

    Added Get request

--- templates/empty/main.js
+++ templates/empty/main.js
@@ -1,6 +1,21 @@
 // This function runs before a transaction.
 const beforeTransaction = async (authorization) => {
-  console.log(authorization);
+  // console.log(authorization);
+  const apiEndpoint = 'https://jsonplaceholder.typicode.com/posts/1';
+
+  const response = await fetch(apiEndpoint, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  const json = await response.json();
+  console.log(json)
+  if (json.UserId === 1) {
+    return true;
+  } else {
+    return false;
+  }
 };
 // This function runs after a transaction was successful.
 const afterTransaction = async (transaction) => {
